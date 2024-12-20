d613d59b813265048f5b0ae10fc432819f07e498
prince agrahari <agrahariprin7991@gmail.com>
2024-12-20 10:52:44 +0530

    returns the sum of multiple numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -15,6 +15,11 @@ RSpec.describe StringCalculator do
     calculator = StringCalculator.new
     expect(calculator.add('1,2')).to eq(3)
   end
+ 
+  it 'returns the sum of multiple numbers' do
+    calculator = StringCalculator.new
+    expect(calculator.add('1,2,3')).to eq(6)
+  end
 
   
 end
